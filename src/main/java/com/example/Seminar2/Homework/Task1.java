e0ec8379b1050eb3654f76e2c60b0c84d51761ba
Daiza <salkodg@gmail.com>
2023-06-11 21:17:02 +0500

    Сделал задание 1 и добавил комментарии

--- src/main/java/com/example/Seminar2/Homework/Task1.java
+++ src/main/java/com/example/Seminar2/Homework/Task1.java
@@ -1,6 +1,7 @@
 package com.example.Seminar2.Homework;
 
 import java.util.HashMap;
+import java.util.Map;
 
 //1) Дана строка sql-запроса "select * from students WHERE ".
 // Сформируйте часть WHERE этого запроса, используя StringBuilder.
@@ -12,26 +13,88 @@ import java.util.HashMap;
 public class Task1 {
     public static void main(String[] args) {
         String request = "select * from students WHERE";
-        String inputStr = "{'name':'Ivanov', 'country':'Russia', 'city':'Moscow', 'age':'null'}";
-        System.out.println(sqlRequest(request, inputStr));
+        String inputStr = "{'name':'Ivan', 'country':'Russia', 'city':'Moscow', 'age':'null'}";
+        //Option1. Решение через конверт inputStr в Map и составления из этого строки.
+        System.out.println(Option1.sqlRequest(request, inputStr));
+        //Option2. Решение через сплит строки. Решение меньше и никакого Map.
+        System.out.println(Option2.sqlRequest(request, inputStr));
     }
-    public static String sqlRequest(String request, String inputStr) {
-//        Составляем SQL запрос.
-        StringBuilder res = new StringBuilder(request);
-        return res.toString();
-    }
-    public static HashMap<String, String> strForHashMap(String inputStr){
 
-        inputStr = inputStr.replace("'","").replace("{","").replace("}","");
+}
+ class Option1 {
+     public static String sqlRequest(String request, String inputStr) {
+         StringBuilder res = new StringBuilder();
+         String sqlReqInputStr = sqlRequestFromHashMap(inputStr);
+         // Если в inputStr все значения будут null, то sqlRequestFromHashMap вернет пустую строку. В result проверка
+         // на строки на пустоту и если строка пустая, то убирает WHERE из request, если нет, то прибавляет к request
+         // результат sqlRequestFromHashMap и конвертит в строку
+         return sqlReqInputStr.isEmpty()
+                 ? request.replaceAll("WHERE", "")
+                 : res.append(request + sqlReqInputStr).toString();
+     }
+
+     public static String sqlRequestFromHashMap(String inputStr) {
+//        Составляем значения в строку для SQL запроса.
+         int count = 0; // итератор, нужен, чтобы правильно выводить лишние AND;
+         StringBuilder res = new StringBuilder();
+         // Делаем Map из inputStr. Я решил так сделать, чтобы в теории можно было пользоваться данными, которые
+         // которые передались в json строке. Сторонние библиотеки решил не подключать
+         Map<String, String> map = strForHashMap(inputStr);
+         for (Map.Entry<String, String> pair : map.entrySet()) {
+             if (!pair.getValue().contains("null") && count == 0) {
+                 count++;
+                 res.append(" " + pair.getKey() + "=" + pair.getValue());
+             } else if (!pair.getValue().contains("null") && count != 0) {
+                 count++;
+                 res.append(" AND " + pair.getKey() + "=" + pair.getValue());
+             }
+         }
+         return res.toString();
+     }
+
+     public static Map<String, String> strForHashMap(String inputStr) {
+         // Множественный .replace, чобы убрать все, неугодные мне, повторяющиеся символы. Способа проще не нашел.
+         inputStr = inputStr.replace("'", "")
+                 .replace("{", "")
+                 .replace("}", "");
+         String[] jsLine = inputStr.split(", ");
+         Map<String, String> map = new HashMap<>();
+         // Итоговый список запихиваю в цикл, который еще раз сплитует, каждое значение строки и кладет ключ и значение
+         // в Map
+         for (String pair : jsLine) {
+             String[] keyValue = pair.split(":");
+             map.put(keyValue[0], keyValue[1]);
+         }
+         return map;
+     }
+ }
+class Option2 {
+    public static String sqlRequest(String request, String inputStr) {
+        StringBuilder res = new StringBuilder();
+        String sqlReqInputStr = ConvStr(inputStr);
+        // Если в inputStr все значения будут null, то sqlRequestFromHashMap вернет пустую строку. В result проверка
+        // на строки на пустоту и если строка пустая, то убирает WHERE из request, если нет, то прибавляет к request
+        // результат sqlRequestFromHashMap и конвертит в строку
+        return sqlReqInputStr.isEmpty()
+                ? request.replaceAll("WHERE", "")
+                : res.append(request + sqlReqInputStr).toString();
+    }
+
+    public static String ConvStr(String inputStr) {
+        // Множественный .replace, чобы убрать все, неугодные мне, повторяющиеся символы. Способа проще не нашел.
+        inputStr = inputStr.replace("'", "")
+                .replace("{", "")
+                .replace("}", "");
         String[] jsLine = inputStr.split(", ");
-        HashMap<String, String> map = new HashMap<>();
+        StringBuilder res = new StringBuilder();
         for (int i = 0; i < jsLine.length; i++) {
-            for (String pair : jsLine) {
-                String[] keyValue = pair.split(":");
-                map.put(keyValue[0], keyValue[1]);
+            String[] keyValue = jsLine[i].split(":");
+            if (!keyValue[1].contains("null") && i == 0) {
+                res.append(" " + keyValue[0] + "=" + keyValue[1]);
+            } else if (!keyValue[1].contains("null") && i != 0) {
+                res.append(" AND " + keyValue[0] + "=" + keyValue[1]);
             }
         }
-        return map;
+        return res.toString();
     }
 }
-
