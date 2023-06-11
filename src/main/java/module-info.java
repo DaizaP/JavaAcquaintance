d4ca2a45b75348ccce5adbc67edde794656f0656
Daiza <salkodg@gmail.com>
2023-06-12 01:25:01 +0500

    Добавил библиотеку Simple Json

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.example.javaacquaintance {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
-                            
+    requires json.simple;
+
     opens com.example.javaacquaintance to javafx.fxml;
     exports com.example.javaacquaintance;
 }
